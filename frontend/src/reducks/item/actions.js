e0a159a7ad69ae229b9c651dd819f213d71d6a6e
demsmo <demsmo@icloud.com>
2022-12-29 18:47:19 -0500

    backend

--- frontend/src/reducks/item/actions.js
+++
@@ -1,7 +0,0 @@
-export const FETCH_ITEM = 'FETCH_ITEM';
-export const fetchItemAction=(item)=>{
-    return {
-        type: 'FETCH_ITEM',
-        payload: item,
-    };
-};
--- frontend/src/reducks/item/selectors.js
+++
@@ -1,5 +0,0 @@
-import { createSelector } from 'reselect';
-
-const ItemsSelector = state => state.item;
-
-export const getItems = createSelector([ItemsSelector], (state) => state.list);
